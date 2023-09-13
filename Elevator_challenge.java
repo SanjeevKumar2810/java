cfff216921b06edac0137306e6ae1a9bc45a1222
SanjeevKumar2810 <102010273+SanjeevKumar2810@users.noreply.github.com>
2023-09-13 11:09:55 +0530

    Add files via upload

---
+++ Elevator_challenge.java
@@ -0,0 +1,22 @@
+package elevator_problem;
+
+import java.util.Scanner;
+
+public class Elevator_challenge 
+{
+	static void automaticElevator() throws InterruptedException {
+		 Elevator elevator = new Elevator();
+		 elevator.lunchtimeElevatorRush();
+		 elevator.start();
+		 }
+		 static void manualElevator() throws InterruptedException {
+		 Elevator elevator = new Elevator();
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("Enter a starting floor 0 - 10");
+		 int start = sc.nextInt();
+		 System.out.println("Enter a destination floor 0 - 10");
+		 int end = sc.nextInt();
+		 
+		 elevator.callElevator(start, end);//calling the elevator to pick us up elevator.start();
+  }
+}
